7b0dcb8cb5ae9144bb31032b81022e0faf256bff
Sthefani Laura <slauraic@hotmail.com>
2018-11-24 18:46:23 -0200

    Criar componente Navbar e Menu

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Menu.css';
+
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            aberto: false
+        }
+    }
+    abreOuFechaMenu = () => {
+
+        if (this.state.aberto) {
+
+            this.setState({ aberto: false });
+
+        } else {
+
+            this.setState({ aberto: true });
+
+        }
+
+    }
+    render() {
+
+        let classesDoBotao = 'navbar-menu__botao';
+        let classesDasOpcoes = 'navbar-menu__opcoes';
+        if (this.state.aberto) {
+            classesDoBotao += ' navbar-menu__botao--aberto';
+            classesDasOpcoes += ' navbar-menu__opcoes--aberto';
+        }
+
+        return (
+            <nav className="navbar-menu">
+                <button onClick={this.abreOuFechaMenu} className={classesDoBotao}>
+                    Menu
+                </button>
+                <ul className={classesDasOpcoes}>
+                    <li>
+                        <NavLink onClick={this.abreOuFechaMenu} to="/quem-somos" activeClassName="navbar-menu__opcoes--ativo">
+                            Quem Somos
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink onClick={this.abreOuFechaMenu} to="/como-funciona" activeClassName="navbar-menu__opcoes--ativo">
+                            Como Funciona
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink onClick={this.abreOuFechaMenu} to="/admirar" className="navbar-admirar__link" activeClassName="navbar-menu__opcoes--ativo">
+                            Admirar Agora!
+                        </NavLink>
+                    </li>
+                </ul>
+                    <button className="navbar-admirar__botao">
+                        Admirar Agora!
+                    </button>
+
+            </nav>
+        )
+    }
+}
+
+export default Menu;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React,  { Component }  from 'react';
+import { Link } from 'react-router-dom';
+import Menu from '../Menu/Menu';
+import logo from './logo.svg';
+import './Navbar.css';
+
+class Navbar extends Component{
+    constructor (props) {
+        super(props);
+    }
+     
+    render (){
+        return (
+            <header className="navbar">
+                <Link to="/">
+                    <img className="navbar__logo" src={logo} alt="Logo"/>
+                </Link>
+                <Menu/>
+
+            </header>
+        )
+    }
+}
+
+
+
+
+export default Navbar;
