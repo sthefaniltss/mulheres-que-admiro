1b279e37d3e8a35f4bf69fa8b57c70941a7b05f7
Sthefani Laura <slauraic@hotmail.com>
2018-11-28 13:34:43 -0200

    Criar formulario na pagina Admirar

--- src/pages/Admirar/Admirar.js
+++ src/pages/Admirar/Admirar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import Label from '../../components/Label/Label';
+import Campo from '../../components/Campo/Campo';
 import ImagePicker from "react-image-picker";
 import passo1 from '../../passo1.svg';
 import "react-image-picker/dist/index.css";
@@ -31,6 +33,19 @@ class Admirar extends Component {
                         <ImagePicker images={layouts} onPick={this.onPick} />
                 </article>
                 <section className="admirar__secao">
+                    <h1>Agora preencha as informações:</h1>
+                    <form>
+                        <Label>Nome da Admirada:</Label>
+                        <Campo id="nome__admirada" type="text" name="nome__admirada" placeholder="Digite aqui o nome da admirada" />
+                        <Label>Mensagem:</Label>
+                        <textarea className="admirar__mensagem" rows="3" placeholder="Digite aqui a sua homenagem"> </textarea>
+                        <Label>Nome completo:</Label>
+                        <Campo id="nome" type="text" name="nome__admirada" placeholder="Digite aqui o seu nome" />
+                        <Label>Data:</Label>
+                        <Campo id="data" type="date" name="data"/>
+                        <Label>Nome na assinatura:</Label>
+                        <Campo id="nome__assinatura" type="text" name="nome__assinatura" placeholder="Digite aqui o nome na assinatura"/>
+                    </form>
                 </section>
             </main>
         </div>
