bbc4be690e599d919d310603456d121e07def267
Sthefani Laura <slauraic@hotmail.com>
2018-12-10 20:28:45 -0200

    Alterar favicon e titúlo do site

---
+++ build/precache-manifest.9cbc1e36f32e8101f2c12fa773f0f0ea.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "abb04169bcc58f74f4048718977792b0",
+    "url": "/static/media/passo3.abb04169.png"
+  },
+  {
+    "revision": "2edf97f99501c75e1ed0",
+    "url": "/static/css/main.618756f1.chunk.css"
+  },
+  {
+    "revision": "cdf41c5ac52dbc3cb459",
+    "url": "/static/js/1.cdf41c5a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df6b0bc702f505a8719d98530b818741",
+    "url": "/static/media/venus.df6b0bc7.svg"
+  },
+  {
+    "revision": "43bc07f78eef9b838ff830a2fb2a53cb",
+    "url": "/static/media/logo.43bc07f7.svg"
+  },
+  {
+    "revision": "453e8ad7d7f83f712ae4459be0ed174c",
+    "url": "/static/media/passo4.453e8ad7.png"
+  },
+  {
+    "revision": "38e4018b813fa3a9ce95fd7357c67068",
+    "url": "/static/media/passo1.38e4018b.svg"
+  },
+  {
+    "revision": "22a2d6a7c9d496b36b722a6a9b02ad0c",
+    "url": "/static/media/passo2.22a2d6a7.png"
+  },
+  {
+    "revision": "2edf97f99501c75e1ed0",
+    "url": "/static/js/main.2edf97f9.chunk.js"
+  },
+  {
+    "revision": "7df62ef9eb11ecb5762e2138dd335227",
+    "url": "/static/media/arabesco.7df62ef9.svg"
+  },
+  {
+    "revision": "377b0587a25b11f259fcfcbb6b87a5a2",
+    "url": "/static/media/venus-pink.377b0587.svg"
+  },
+  {
+    "revision": "92d0266a8913fe842ad930cf550562f4",
+    "url": "/static/media/layout2.92d0266a.png"
+  },
+  {
+    "revision": "c730c98910567a4e843f7e78c2750e25",
+    "url": "/static/media/layout3.c730c989.png"
+  },
+  {
+    "revision": "6df6d46a595c01707a40974108ef3d20",
+    "url": "/static/media/fundo.6df6d46a.svg"
+  },
+  {
+    "revision": "4bb56d883ba86c8e0da11b7220fb21a0",
+    "url": "/static/media/checked.4bb56d88.svg"
+  },
+  {
+    "revision": "629610dc9758999d1df9c57136d47782",
+    "url": "/static/media/cancel.629610dc.svg"
+  },
+  {
+    "revision": "5d2d251070eac57f1b9d3121c21d868c",
+    "url": "/static/media/download-arrow.5d2d2510.svg"
+  },
+  {
+    "revision": "b0f65522fec70e6c4d923934b50c0956",
+    "url": "/index.html"
+  }
+];
